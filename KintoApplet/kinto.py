cc2952aa32d0d6274300ad6bc8717bd2f85309ed
Ben Reaves <rbreaves@gmail.com>
2020-07-14 23:29:29 -0500

    - Added suspend and change kb type, WIP

--- KintoApplet/kinto.py
+++ KintoApplet/kinto.py
@@ -17,14 +17,14 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import subprocess,gi.repository
+import subprocess,time,os,gi.repository
 gi.require_version('Budgie','1.0')
 gi.require_version('Gtk','3.0')
 from gi.repository import Budgie, GObject, Gtk
 
-class BudgieRestart(GObject.GObject, Budgie.Plugin):
+class Kinto(GObject.GObject, Budgie.Plugin):
 
-	__gtype_name__ = "BudgieRestart"
+	__gtype_name__ = "Kinto"
 
 	def __init__(self):
 
@@ -32,10 +32,14 @@ class BudgieRestart(GObject.GObject, Budgie.Plugin):
 
 	def do_get_panel_widget(self, uuid):
 
-		return BudgieRestartApplet(uuid)
+		return KintoApplet(uuid)
 
 
-class BudgieRestartApplet(Budgie.Applet):
+
+
+class KintoApplet(Budgie.Applet):
+
+	homedir = os.path.expanduser("~")
 
 	box = Gtk.EventBox()
 	revealer=Gtk.Revealer()                      # Contains individual restart actions
@@ -43,51 +47,78 @@ class BudgieRestartApplet(Budgie.Applet):
 	button.set_relief(Gtk.ReliefStyle.NONE)
 	button_arrow=Gtk.Button()                    # Down arrow
 	button_arrow.set_relief(Gtk.ReliefStyle.NONE)
+	img = Gtk.Image.new_from_icon_name("kinto-invert", Gtk.IconSize.BUTTON)
 	img_arrow=Gtk.Image.new_from_icon_name('pan-down-symbolic',Gtk.IconSize.BUTTON)
 	revealed=False                               # Whether Gtk.Revealer is open or not.
+	button_suspend = Gtk.Button()
+	button_winmac = Gtk.Button()
+	button_setscale = Gtk.Button()
+	button_suspend.set_relief(Gtk.ReliefStyle.NONE)
+	button_winmac.set_relief(Gtk.ReliefStyle.NONE)
+	button_setscale.set_relief(Gtk.ReliefStyle.NONE)
 
 	def __init__(self,uuid):
 
 		Budgie.Applet.__init__(self)
+		# self.menu = Gtk.Menu()
 		self.initUI()
 
 	def initUI(self):
 
 		self.popover = Budgie.Popover.new(self.box)
-		self.img = Gtk.Image.new_from_icon_name("budgie-restart", Gtk.IconSize.BUTTON)
-		self.button_panel = Gtk.Button(label='Restart Panel')
-		self.button_wm = Gtk.Button(label='Restart WM')
+		# self.img = Gtk.Image.new_from_icon_name("kinto-invert", Gtk.IconSize.BUTTON)
+		# self.button_panel = Gtk.Button(label='Restart Panel')
+		# self.button_wm = Gtk.Button(label='Restart WM')
 		seperator = Gtk.Separator(orientation=Gtk.Orientation.VERTICAL)
 		self.hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)         # To store the Reset(Both) button and arrow
 		self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)           # Main VBox
 		self.vbox_buttons = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)   # Vbox inside revealer
 
-		self.box.set_tooltip_text("Restart Budgie Desktop")
+		self.box.set_tooltip_text("Kinto")
+
+		res = subprocess.Popen(['sudo', 'systemctl','is-active','--quiet','xkeysnail'])
+		res.wait()
+		
+		if res.returncode == 0:
+			# self.button_suspend = Gtk.Button("Suspend Kinto")
+			self.button_suspend.set_label("Suspend Kinto")
+			self.img.set_from_icon_name("kinto-invert", Gtk.IconSize.BUTTON)
+			self.button_suspend.connect("clicked",self.suspend,True)
+		else:
+			# self.button_suspend = Gtk.Button("Enable Kinto")
+			self.button_suspend.set_label("Enable Kinto")
+			self.img.set_from_icon_name("kinto", Gtk.IconSize.BUTTON)
+			self.button_suspend.connect("clicked",self.suspend,False)
+		self.vbox.pack_start(self.button_suspend,True,False,5)
+		self.vbox.pack_start(seperator,True,False,0)
+
 		self.box.add(self.img)
 		self.box.connect("button-press-event", self.on_press)
-
 		self.add(self.box)
 
-		self.button_panel.set_relief(Gtk.ReliefStyle.NONE)
-		self.button_wm.set_relief(Gtk.ReliefStyle.NONE)
-		self.button_wm.connect("clicked",self.restart,False,True)
-		self.button_panel.connect("clicked",self.restart,True,False)
-		self.vbox_buttons.pack_start(self.button_panel,False,False,0)
-		self.vbox_buttons.pack_start(self.button_wm,False,False,0)
+		command = "perl -ne 'print if /(#.*)(# Mac)\n/' ~/.config/kinto/kinto.py | wc -l"
+		res = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
+		res.wait()
+		res = res.communicate()[0]
 
+		if True:
+			self.button_winmac.set_label("Set Win/Mac KB Type")
+			self.button_winmac.connect("clicked",self.setKB,"winmac")
+		else:
+			self.button_winmac.set_label("Set Mac Only KB Type")
+			self.button_winmac.connect("clicked",self.setKB,"mac")
+		self.vbox.pack_start(self.button_winmac,True,False,5)
+		self.vbox.pack_start(seperator,True,False,0)
+		# self.hbox.pack_end(self.button_arrow,False,False,5)
 
-		self.button.set_label("Restart Desktop")
-		self.button.connect("clicked",self.restart,True,True)
-		self.hbox.pack_start(self.button,True,False,5)
-		self.hbox.pack_start(seperator,True,False,0)
-		self.hbox.pack_end(self.button_arrow,False,False,5)
-
-		self.button_arrow.set_image(self.img_arrow)
-		self.button_arrow.connect("clicked",self.revealer_show)
-
-
-		self.revealer.set_reveal_child(False)
-		self.revealer.add(self.vbox_buttons)
+		# if res.returncode == 0:
+		# 	self.button_setscale.set_label("Set 1x DPI Scale")
+		# 	self.button_setscale.connect("clicked",self.setScale,1)
+		# else:
+		# 	self.button_setscale.set_label("Set 2x DPI Scale")
+		# 	self.button_setscale.connect("clicked",self.setScale,2)
+		# self.vbox.pack_start(self.button_setscale,True,False,5)
+		# self.vbox.pack_start(seperator,True,False,0)
 
 		self.vbox.pack_start(self.hbox,False,False,5)
 		self.vbox.pack_start(self.revealer,False,False,0)
@@ -142,18 +173,69 @@ class BudgieRestartApplet(Budgie.Applet):
 			self.change_icon()
 			self.revealer.set_reveal_child(True)
 
-
-	def restart(self,button,panel,wm):
-		""" Restart the panel,WM or both """
-
+	def suspend(self,button,suspendKinto):
 		try:
-			if wm:
-				subprocess.Popen(['nohup','budgie-wm', '--replace','&'])
+			if suspendKinto:
+				subprocess.Popen(['sudo', 'systemctl','stop','xkeysnail'])
+				self.button_suspend.set_label("Enable Kinto")
+				self.img.set_from_icon_name("kinto", Gtk.IconSize.BUTTON)
+				self.button_suspend.connect("clicked",self.suspend,False)
+				self.box.add(self.img)
 
-			if panel:
-				subprocess.Popen(['nohup','budgie-panel', '--replace','&'])
+			else:
+				subprocess.Popen(['sudo', 'systemctl','restart','xkeysnail'])
+				self.button_suspend.set_label("Suspend Kinto")
+				self.img.set_from_icon_name("kinto-invert", Gtk.IconSize.BUTTON)
+				self.button_suspend.connect("clicked",self.suspend,True)
+				self.box.add(self.img)
 
 		except subprocess.CalledProcessError:                                  # Notify user about error on running restart commands.
-			subprocess.Popen(['notify-send','Budgie Restart: Error !','-i','budgie-desktop-symbolic'])
+			subprocess.Popen(['notify-send','Kinto: Error Suspending!','-i','budgie-desktop-symbolic'])
+
+	def setKB(self,button,kbtype):
+		kconfig = self.homedir+"/.config/kinto/kinto.py"
+		try:
+			if kbtype == "winmac":
+				label = "Set Mac KB Type"
+				connect = "mac"
+
+				setwinmac = ['s/^(\s{3})(\s{1}#)(.*# WinMac\n|.*# WinMac -)|^(?!\s{4}#)(\s{3})(\s{1})(.*)( # )(Mac\n|Mac -)/   $3$7$6$7$8/g']
+
+				# # Comment out Mac
+				# unset1 = ['perl','-pi','-e','s/^(?!\s{4}#)(\s{4})(.*# Mac\n)/    # $2/g',kconfig]
+				# unset2 = ['perl','-pi','-e','s/^(?!\s{4}#)(\s{4})(.*# Mac - Multi-language \(Remove\)\n)/    # $2/g',kconfig]
+
+				# # Enable WinMac
+				# reset1 = ['perl','-pi','-e','s/(\s{4}# )(.*# WinMac - Multi-language \(Remove\)\n)/    $2/g',kconfig]
+				# reset2 = ['perl','-pi','-e','s/(\s{4}# )(.*# WinMac\n)/    $2/g',kconfig]
+			else:
+				label = "Set Win/Mac KB Type"
+				connect = "winmac"
+
+				setwinmac = ['s/^(\s{3})(\s{1}#)(.*# Mac\n|.*# Mac -)|^(?!\s{4}#)(\s{3})(\s{1})(.*)( # )(WinMac)/   $3$7$6$7$8/g']
+
+				# # Comment out WinMac
+				# unset1 = ['perl','-pi','-e','s/^(?!\s{4}#)(\s{4})(.*# WinMac\n)/    # $2/g',kconfig]
+				# unset2 = ['perl','-pi','-e','s/^(?!\s{4}#)(\s{4})(.*# WinMac - Multi-language \(Remove\)\n)/    # $2/g',kconfig]
+
+				# # Enable Mac
+				# reset1 = ['perl','-pi','-e','s/(\s{4}# )(.*# Mac - Multi-language \(Remove\)\n)/    $2/g',kconfig]
+				# reset2 = ['perl','-pi','-e','s/(\s{4}# )(.*# Mac\n)/    $2/g',kconfig]
+
+			restart = ['nohup','sudo', 'systemctl','restart','xkeysnail']
+			cmds = ['perl','-pi','-e']+setwinmac+[kconfig]
+
+			subprocess.Popen(cmds)
+			time.sleep(0.5)
+			subprocess.Popen(restart)
+
+			self.button_winmac.set_label(label)
+			self.button_winmac.connect("clicked",self.setKB,connect)
+
+		except subprocess.CalledProcessError:                                  # Notify user about error on running restart commands.
+			subprocess.Popen(['notify-send','Kinto: Error Reseting KB Type!','-i','budgie-desktop-symbolic'])
+
+	def setScale(self,button,scale):
+		print("scale")
 
 #END
